5f1e0dee06bcc0b3d47c3c4be764861a43c1a0ad
jagath-sajjan <jagathsajjan097@gmail.com>
2026-02-05 20:10:17 +0530

    fixed cachein
    removed revalidate, tried http cache worked so implemented the same for rest + added bbox params

--- app/api/bmtc/aggregated/route.ts
+++ app/api/bmtc/aggregated/route.ts
@@ -1,16 +1,75 @@
-export async function GET() {
+import { NextRequest } from "next/server";
+import { features } from "process";
+
+export async function GET(req: NextRequest) {
+    const bboxParam = req.nextUrl.searchParams.get("bbox"); // gets params
+
     const res = await fetch(process.env.BMTC_AGGREGATED_URL!, {
-        next: { revalidate: 864000 } // 24h cache
-    });
+        cache: "no-store" // http cachin
+    })
 
     if (!res.ok) {
         return new Response("Failed To Fetch AggStops", { status: 500 });
     }
 
-    return new Response(await res.text(), {
-        headers: {
-            "Content-Type": "applications/json",
-            "Cache-Control": "public,max-age=86400"
-        }
+    // prase geojson
+    const geojson = await res.json();
+
+    // no bbox fallback dump
+    if (!bboxParam) {
+        return Response.json(geojson, {
+            headers: {
+                "Cache-Control": "public, max-age=3600"
+            }
+        });
+    }
+    const [minLng, minLat, maxLng, maxLat] = bboxParam.split(",").map(Number);
+
+    // validate bbox
+    // if value NaN, bbox invalid
+    if ([minLng, minLat, maxLng, maxLat].some(Number.isNaN)) {
+        return new Response("Invaild bbox", { status: 400 });
+    }
+
+    // filtering geojson
+    const filteredFeatures = geojson.features.filter((feature: any) => {
+        const [lng, lat] = feature.geometry.coordinates;
+        return (
+            lng >= minLng &&
+            lng <= maxLng &&
+            lat >= minLat &&
+            lat <= maxLat
+        ); 
     });
+
+    // filtered features return
+    return Response.json(
+        {
+            type: "FilterdCollection",
+            features: filteredFeatures
+        },
+        {
+            headers: {
+                "Cache-Control": "public, max-age=3600"
+            }
+        }
+    );
 }
+
+// export async function GET() {
+//     const res = await fetch(process.env.BMTC_AGGREGATED_URL!, {
+//         cache: "no-store" // http cache
+//         // next: { revalidate: 864000 } // 24h cache
+//     });
+
+//     if (!res.ok) {
+//         return new Response("Failed To Fetch AggStops", { status: 500 });
+//     }
+
+//     return new Response(await res.text(), {
+//         headers: {
+//             "Content-Type": "applications/json",
+//             "Cache-Control": "public,max-age=86400, stale-while-revalidate=43200"
+//         }
+//     });
+// }
--- app/api/bmtc/routes/route.ts
+++ app/api/bmtc/routes/route.ts
@@ -1,16 +1,70 @@
-export async function GET() {
-    const res = await fetch(process.env.BMTC_ROUTES_URL !, {
-        next: { revalidate: 864000 } // 24h cache
+import { NextRequest } from "next/server";
+import { features } from "node:process";
+import { resumeToPipeableStream } from "react-dom/server";
+
+export async function GET(req: NextRequest) {
+    const bboxParam = req.nextUrl.searchParams.get("bbox");
+
+    const res = await fetch(process.env.BMTC_ROUTES_URL!, {
+        cache: "no-store"
     })
 
     if (!res.ok) {
-        return new Response("Failed To Fetch Routes", { status: 500 });
+        return new Response("Failed TO Fetch Routes", { status: 500 });
     }
 
-    return new Response(await res.text(), {
-        headers: {
-            "Content-Type": "application/json",
-            "Cache-Control": "public,max-age=86400"
-        }
+    const geojson = await res.json();
+
+    if (!bboxParam) {
+        return Response.json(geojson, {
+            headers: {
+                "Cache-Control": "public, max-age=3600"
+            }
+        });
+    }
+    const [minLng, minLat, maxLng, maxLat] = bboxParam.split(",").map(Number);
+
+    if ([minLng, minLat, maxLng, maxLat].some(Number.isNaN)) {
+        return new Response("Invalid bbox", { status: 400 });
+    }
+
+    const filteredFeatures = geojson.features.filter((feature: any) => {
+        const [lng, lat] = feature.geomentry.coordinates;
+        return (
+            lng >= minLng &&
+            lng <= maxLng &&
+            lat >= minLat &&
+            lat <= minLat
+        );
     });
+
+    return Response.json(
+        {
+            type: "FiltredCollection",
+            features: filteredFeatures
+        },
+        {
+            headers: {
+                "Cache-Control": "public, max-age=3600"
+            }
+        }
+    );
 }
+
+// export async function GET() {
+//     const res = await fetch(process.env.BMTC_ROUTES_URL !, {
+//         cache: "no-store" // http cachin
+//        // next: { revalidate: 864000 } // 24h cache
+//     })
+
+//     if (!res.ok) {
+//         return new Response("Failed To Fetch Routes", { status: 500 });
+//     }
+
+//     return new Response(await res.text(), {
+//         headers: {
+//             "Content-Type": "application/json",
+//             "Cache-Control": "public,max-age=86400, stale-while-revalidate=43200"
+//         }
+//     });
+// }
